0fb33613434a35c9ab22033a16ef8397479a33f2
龍希 我妻 <190312@jc-21.jp>
2021-12-02 13:57:02 +0900

    変更1

--- R03BMI/MainPage.xaml.cs
+++ R03BMI/MainPage.xaml.cs
@@ -18,6 +18,45 @@ namespace R03BMI
 
         private void Button_Clicked(object sender, EventArgs e)
         {
+            string w = "weighit";
+            String h = "heighit";
+            double bmi;
+
+            try
+            {
+                double we = parse.double(w);
+                double he = parse.double(h);
+
+                if (we > 200)
+                {
+                    we=we/1000;
+                    //g→kg
+                }
+                else
+                {
+                    //kg
+                }
+
+                if (he > 2.5)
+                {
+                    we=we/100;
+                    //cm→m
+                }
+                else
+                {
+                    //m
+                }
+
+                bmi = we /(he*he);
+                result.Text=bmi.Tostring();
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+
 
         }
     }
